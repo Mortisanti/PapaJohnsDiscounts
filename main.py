c5f4af99c5bc4d47371805231f863ae58b50cc2b
Mortisanti <moltigamer@gmail.com>
2021-03-31 23:55:41 -0400

    Working product

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-#TODO Check date of last win, compare to today's date (give or take a day or two)
 import requests
 from bs4 import BeautifulSoup as BS
-from requests.models import Response
 from datetime import datetime
-
-"""
-# JUST REALIZED THIS WAS USELESS. Use built-in method to determine difference between dates, compare result to 1 or 2 day difference
-# Get today's date; do not format as to keep integer values
-today = datetime.now()
-print(f'{today.month}/{today.day}')
-
-# Get last win date, convert to datetime object so that the values are integers
-last_win_date = '03/29 23:30'
-last_win_date_converted = datetime.strptime(last_win_date, '%m/%d %H:%M')
-print(f'{last_win_date_converted.month}/{last_win_date_converted.day}')
-"""
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'}
 url = 'https://betsapi.com/t/57721/MIA-Heat'
@@ -25,24 +11,38 @@ def get_latest_game():
     tables = soup.find_all('table')
     rows = tables[1].find_all('tr')
     columns = rows[0].find_all('td')
-    latest_date = columns[1]['data-dt']
-    # Outcome can either be W (win), L (loss), - (postponed), or D (draw)
-    latest_outcome = columns[4].text
-    return latest_date, latest_outcome
+    game_date = columns[1]['data-dt']
+    outcome = columns[4].text
+    return game_date, outcome
 
-def convert(latest_date):
-    today = datetime.now().strftime("%Y-%m-%d")
-    latest_date_formatted = datetime.strptime(latest_date, '%Y-%m-%dT%H:%M:%SZ')
-    latest_date_converted = latest_date_formatted.strftime("%Y-%m-%d")
-    return latest_date_converted
+def format_and_parse_dates(game_date):
+    today_string = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    todays_date_parsed = datetime.strptime(today_string, '%Y-%m-%dT%H:%M:%SZ')
+    games_date_parsed = datetime.strptime(game_date, '%Y-%m-%dT%H:%M:%SZ')
+    return todays_date_parsed, games_date_parsed
 
-def compare():
-    return
+def get_seconds_since_win(todays_date_parsed, games_date_parsed):
+    difference = abs(todays_date_parsed - games_date_parsed)
+    seconds_since_win = (difference.days * 86400) + difference.seconds
+    return seconds_since_win
 
-# Maybe check response headers too
+def compare(outcome):
+    thirty_six_hours = 129600
+    if outcome == 'W':
+        if get_seconds_since_win(todays_date_parsed, games_date_parsed) < thirty_six_hours:
+            result = "The Miami Heat recently won! Promo code HEATWIN is valid."
+    elif outcome == 'L':
+        result = "The Miami Heat recently lost."
+    elif outcome == '-':
+        result = "The Miami Heat's latest game was postponed."
+    elif outcome == 'D':
+        result = "The Miami Heat's latest game ended in a draw."
+    return result
+
 response = requests.get(url, headers=headers)
 if response.status_code == 200:
-    latest_date, latest_outcome = get_latest_game()
-    latest_date_converted = convert(latest_date)
+    game_date, outcome = get_latest_game()
+    todays_date_parsed, games_date_parsed = format_and_parse_dates(game_date)
+    print(compare(outcome))
 else:
     print("No response from webpage.")
